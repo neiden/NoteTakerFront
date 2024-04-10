2e0e2431a14edd865802e88f9d18b628a30a8168
neiden <pinkgodzela@gmail.com>
2024-04-10 13:30:46 -0700

    Added registration

--- script.js
+++ script.js
@@ -30,5 +30,38 @@ function login() {
 }
 
 
+function register(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    fetch('https://localhost:7089/User/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.token) {
+            console.log('Registration successful');
+            console.log('Data:', data, 'User ID:', data.userId, 'Token:', data.token);
+            localStorage.setItem('token', data.token);
+        } else {
+            console.error('Registration failed');
+        }
+    })
+    .catch(error => {
+        console.error('Network error', error);
+    });
+
+}
+
+
 const loginButton = document.getElementById('login-button');
 loginButton.addEventListener('click', login);
+
+const registerButton = document.getElementById('register-button');
+registerButton.addEventListener('click', register);
