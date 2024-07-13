1d5fbd5bb15e3234d3ba09722fac335fb9921382
neiden <pinkgodzela@gmail.com>
2024-07-13 12:13:31 -0700

    Fixed css for home component

--- TokenTest/src/app/verify-account/verify-account.component.ts
+++ TokenTest/src/app/verify-account/verify-account.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-verify-account',
   templateUrl: './verify-account.component.html',
-  styleUrls: ['./verify-account.component.css']
+  styleUrls: ['./verify-account.component.css'],
+  standalone: true
 })
 export class VerifyAccountComponent {
-
+  constructor() { }
 }
